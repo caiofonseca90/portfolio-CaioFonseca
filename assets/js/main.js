1afebf9dfc1859db8e585df43903a8271273ac25
Caio Fonseca <caiofonseca1990@gmail.com>
2023-04-13 10:23:13 -0300

    haeder 90% finalizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,34 @@
 const menuiten = document.querySelector('.menu-lines');
 const NavMenu = document.querySelector('.nav-menu')
+const navbar = document.querySelector('.nav-container');
+const menuLink = document.querySelectorAll('.nav-menu a[href^="#"]')
+
+// função que leva até a section clicada pelo atributo referenciado na função " setDistanceTop"
+function scrollToSection(evento) {
+    evento.preventDefault(); // previne o padrao de alterar as # no link da pagina
+    const distanceTop = setDistanceTop(evento.target) -100;
+    nativeScroll(distanceTop);
+};
+
+function setDistanceTop(elemento) {
+    const id = elemento.getAttribute("href");
+    return document.querySelector(id).offsetTop;
+};
+// 
+function nativeScroll(setDistanceTop) { 
+    window.scroll({
+        top:setDistanceTop,
+        behavior:"smooth",
+    });   
+};
+
+
+menuLink.forEach((link ) =>{ 
+    link.addEventListener('click', scrollToSection);
+});
+
+
+
 
 
 // menu toggle
